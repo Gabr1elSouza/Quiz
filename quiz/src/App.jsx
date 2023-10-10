6bbc574d83a0998e9ec2d2592621102fa5782ee7
Gabriel Souza <gablgdario03@gmail.com>
2023-10-10 15:27:29 -0300

    welcome page and css

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,10 +1,14 @@
+//react, componentes, estaticos
+import Welcome from './components/Welcome'
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Quiz app</h1>
+      <h1>Quiz de Programação</h1>
+      <Welcome/>
     </div>
   )
 }

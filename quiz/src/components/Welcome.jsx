15fcae577b73ad0cee8e6de80e44afa92d4a55c6
Gabriel Souza <gablgdario03@gmail.com>
2023-10-11 15:59:41 -0300

    useReducer

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -5,14 +5,13 @@ import { QuizContext } from '../context/quiz'
 import { useContext } from 'react'
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext)
+  const [quizState, dispatch] = useContext(QuizContext)
 
-  console.log(quizState)
   return (
     <div id="welcome">
         <h2>Seja bem vindo</h2>
         <p>Cliquie no botão abaixo para começar:</p>
-        <button>Iniciar</button>
+        <button onClick={()=> dispatch({type: "CHANGE_STATE" })}>Iniciar</button>
         <img src={Quiz} alt="Inicio do quiz"></img>
     </div>
   )

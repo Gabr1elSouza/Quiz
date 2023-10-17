eb831d94ea134e7d7edfd6f5fe430304498c087a
Gabriel Souza <gablgdario03@gmail.com>
2023-10-17 15:08:07 -0300

    Restart button and score

--- quiz/src/components/GameOver.jsx
+++ quiz/src/components/GameOver.jsx
@@ -5,14 +5,17 @@ import wellDone from '../img/welldone.svg'
 
 import './GameOver.css'
 function GameOver () {
+
+  const [quizState, dispatch] = useContext(QuizContext)
+
   return (
     <div>
         <div id="gameover">
             <h2>Fim de jogo</h2>
-            <p>Pontuação: x</p>
-            <p>Você acertou y de z perguntas.</p>
+            <p>Pontuação: {quizState.score}</p>
+            <p>Você acertou {quizState.score} de {quizState.questions.length}{" "} perguntas.</p>
             <img src={wellDone} alt='gameover'/>
-            <button>
+            <button onClick={()=> dispatch({type: "NEW_GAME"})}>
                 Reiniciar
             </button>
         </div>
